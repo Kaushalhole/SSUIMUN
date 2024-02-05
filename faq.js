6316be7aca5f808c717a2d795eecc39b5271ce11
Kaushalhole <holekaushal@gmail.com>
2024-02-05 23:05:46 +0530

    faq

---
+++ faq.js
@@ -0,0 +1,172 @@
+let isOpen = false;
+let faq1 = document.getElementById("faq_1");
+
+faq1.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } 
+    })
+    faq1.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq1.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq2 = document.getElementById("faq_2");
+
+faq2.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    })
+    faq2.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq2.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+
+let faq3 = document.getElementById("faq_3");
+
+faq3.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    });
+    faq3.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq3.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq4 = document.getElementById("faq_4");
+
+faq4.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    })
+    faq4.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq4.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq5 = document.getElementById("faq_5");
+
+faq5.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+
+    });
+    faq5.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq5.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq6 = document.getElementById("faq_6");
+
+faq6.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    });
+    faq6.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq6.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq7 = document.getElementById("faq_7");
+
+faq7.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    });
+    faq7.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq7.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq8 = document.getElementById("faq_8");
+
+faq8.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    });
+    faq8.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq8.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
+
+let faq9 = document.getElementById("faq_9");
+
+faq9.addEventListener("click", function() {
+    if(isOpen) {
+    document.querySelectorAll('.video-card').forEach(c => {
+        if(c.style.display === "block"){
+        c.style.display = 'none'
+        } else {
+        }
+    });
+    faq9.querySelector('.video-card').style.display = 'block';
+
+    } else {
+    faq9.querySelector('.video-card').style.display = 'block';
+
+    }
+    isOpen = !isOpen;
+})
